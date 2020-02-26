b020ed46d3997cbf5f8089e2b1387be9335aee16
Bang Nguyen <duybang140494@gmail.com>
2020-02-26 15:17:11 +0700

    updated

---
+++ Output Export/Cfg01/001_test_RBAPLPLANT_ResetCSAIInterface.h
@@ -0,0 +1,6 @@
+struct CPPTH_LOOP_INPUT_STRUCT CPPTH_LOOP_INPUT[] = {
+
+    {"Sheet:001,Case:001", "RBAPLPLANT_ResetCSAIInterface_001", "", 1
+
+, 0, 0},
+};
---
+++ Output Export/dummy/Template_test_.h
@@ -0,0 +1,6 @@
+struct CPPTH_LOOP_INPUT_STRUCT CPPTH_LOOP_INPUT[] = {
+
+    {"Dummy Test Case", "Dummy Test Case", "", 0
+
+},
+};
