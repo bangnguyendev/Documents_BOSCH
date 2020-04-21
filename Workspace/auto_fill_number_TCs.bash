2c5a47eed88bf25a0169f30d57140a0ab83a4234
duybang140494 <duybang140494@gmail.com>
2020-04-21 07:59:48 +0700

    ok

--- Workspace/auto_fill_number_TCs.bash
+++ Workspace/auto_fill_number_TCs.bash
@@ -1,24 +1,36 @@
 #!bin/bash
 
 
-count=`grep -i '"test_' atest_zxc.c -c`
+count=`grep 'START TEST: ".*\_' atest_zxc.c -c`
 temp_count=1
+grep 'START TEST: ".*\_' atest_zxc.c| cut -d ' ' -f3 | cut -d '_' -f1 > temp_out1
+#cat temp_out1
 
-for i in `grep -i '"test_' atest_zxc.c`
+for i in `cat temp_out1`
 do
-	if [$temp_count != $count]
-	then
-		echo $i | sed "s/test_/$temp_count: /"
-		temp_count=$[temp_count+1]
-		echo $temp_count
-		echo ==========
-	else
-	
-		echo done
-	fi
+	echo day la $i
 
-	#echo $temp_count
-	#echo $i | sed "s/test_/$temp_count: /"
-	#temp_count=$[temp_count+1]
+done 
+
+
+exit
+for i in ``
+do
+	echo $i		
+	if (( $temp_count <= $count))
+	then
+		echo $temp_count
+		echo $i
+		
+		
+		teamp_2='START TEST: "$temp_count:'
+
+	  	echo $i | sed 's/$i/$teamp_2/g'
+		((temp_count++))
+		
+		echo ==========
+	fi	
+	
 	
 done
+
