ff3891e2aeb45d350c5ae4f3047577342001f12b
duybang140494 <duybang140494@gmail.com>
2020-04-21 07:59:48 +0700

    ok

--- BangNguyen/add_file.sh
+++ BangNguyen/add_file.sh
@@ -1,4 +1,4 @@
-#1/bin/bash
+#!/bin/bash
 read -p "=>> Nhap DataBase: " name_DataBase
 read -p "=>> Nhap PATH: " name_PATH
 read -p "=>> Nhap file: " name_var
@@ -6,10 +6,10 @@ read -p "=>> Nhap file: " name_var
 for i in {0..18} {18..0} ; do echo -en "\e[48;5;${i}m \e[0m" ; done ; echo
 echo -e "\e[0m"
 
-find |grep -i "$name_DataBase" |grep -i "$name_PATH" |grep -i "$name_var$"
+#find -iname '*.c' |grep -i "$name_DataBase" |grep -i "$name_PATH" |grep -i "$name_var.c$"
 
-file=`find |grep -i "$name_DataBase" |grep -i "$name_PATH" |grep -i "$name_var$" `
-count=`find |grep -i "$name_DataBase" |grep -i "$name_PATH" |grep -i "$name_var$" -c`
+file=`find |grep -i "$name_DataBase" |grep -i "$name_PATH" |grep -i "$name_var.c$" `
+count=`find |grep -i "$name_DataBase" |grep -i "$name_PATH" |grep -i "$name_var.c$" -c`
 echo -e "\e[30;48;5;82m Luong File tim thay: $count \e[0m"
 if [ $count -eq 1 ]
 then
@@ -19,7 +19,7 @@ then
 	then
 		cp -r ./hdr $name_var
 		cp stub.c $name_var 
-		cp `find |grep -i "$name_DataBase" |grep -i "$name_PATH" |grep -i "$name_var$"` $name_var	
+		cp `find |grep -i "$name_DataBase" |grep -i "$name_PATH" |grep -i "$name_var$"` $name_var.c	
 		echo -e "\e[1;38;5;46m$file found. "
 		echo -e "\e[30;48;5;82m ===Done=== \e[0m"
 	else
