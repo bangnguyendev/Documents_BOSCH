ff134c4d1ea966fb3e6b9c5821cb117664008526
duybang140494 <duybang140494@gmail.com>
2020-04-22 08:29:45 +0700

    updated scrip

--- Workspace/_Scrip_add_numberTCs/auto_fill_number_TCs.sh
+++ Workspace/_Scrip_add_numberTCs/auto_fill_number_TCs.sh
@@ -1,11 +1,13 @@
 #!bin/bash
 read -p "vui long nhap n(number)/t(test_): " var_type
 read -p "vui long nhap name_file: " name_file
-
+rm -rf log_change.txt
+touch log_change.txt
 temp_count=1
 
 if [ $var_type == "n" ]
 then
+	echo Chon kieu number >> log_change.txt
 	echo Chon kieu number
 	# temp_out1 export nhung chuoi can thay doi #at_line_code
 	grep 'START TEST: "[0-9]' $name_file.c| cut -d ' ' -f3 > temp_out1
@@ -13,8 +15,8 @@ then
 	grep -n 'START TEST: "[0-9]' $name_file.c| cut -d ':' -f1 > temp_out2
 elif [ $var_type == "t" ]
 then
-	echo Chon kieu test_
-	
+	echo Chon kieu test_ >> log_change.txt
+	echo Chon kieu test_ 
 	grep 'START TEST: ".*\_' $name_file.c| cut -d ' ' -f3 > temp_out1
 	# them -n la lay #at_line_code can thay doi
 	grep -n 'START TEST: ".*\_' $name_file.c| cut -d ':' -f1 > temp_out2
@@ -35,11 +37,15 @@ do
 	fi
 	
 	string_da_thay=`printf 'START TEST: "%s: ' $temp_count`
-	echo $string_can_thay
+	echo $string_can_thay >> log_change.txt
+	echo "===========> $string_da_thay" >> log_change.txt
+	echo $string_can_thay 
 	echo "===========> $string_da_thay"
 	#sed -i "$i s/$string_can_thay/$string_da_thay/" $name_file.c
 	((temp_count++))
 done 
 rm -rf temp_out1
 rm -rf temp_out2
+read -p "Close sau 3s... Xem lai o log_change.txt" -t 3
+
 
