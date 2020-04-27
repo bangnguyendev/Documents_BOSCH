89ea94cee8511ae1025c35d01f1c046f19176140
Nguyen Duy Bang <duybang140494@gmail.com>
2020-04-27 23:12:52 +0700

    ok

---
+++ BangNguyen/THUMUC_TESTVENHA/Hoc_bash/DcmAppl_22_rdbi/hdr/empty/NET_PBCfg_SoftwarePartNumber.h
@@ -0,0 +1,14 @@
+/*
+ * NET_PBCfg_SoftwarePartNumber.h
+ *
+ *  Created on: Apr 22, 2020
+ *      Author: nbg7hc
+ */
+
+#ifndef HDR_NET_PBCFG_SOFTWAREPARTNUMBER_H_
+#define HDR_NET_PBCFG_SOFTWAREPARTNUMBER_H_
+
+
+#include "include.h"
+
+#endif /* HDR_NET_PBCFG_SOFTWAREPARTNUMBER_H_ */
---
+++ BangNguyen/THUMUC_TESTVENHA/Hoc_bash/DcmAppl_22_rdbi/hdr/empty/RBSUPPLYNBS_AverageVoltage.h
@@ -0,0 +1,14 @@
+/*
+ * RBSUPPLYNBS_AverageVoltage.h
+ *
+ *  Created on: Apr 22, 2020
+ *      Author: nbg7hc
+ */
+
+#ifndef HDR_RBSUPPLYNBS_AVERAGEVOLTAGE_H_
+#define HDR_RBSUPPLYNBS_AVERAGEVOLTAGE_H_
+
+
+#include "include.h"
+
+#endif /* HDR_RBSUPPLYNBS_AVERAGEVOLTAGE_H_ */
