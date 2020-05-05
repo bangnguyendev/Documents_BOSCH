69a07ce0b0e6e4faf4cbdd1a84c445b67f600207
EXTERNAL Nguyen Duy Bang (Ban Vien <nbg7hc@bosch.com>
2020-05-05 14:40:24 +0700

    :ok

--- Workspace/_Scrip_add_numberTCs/auto_fill_number_TCs.sh
+++ Workspace/_Scrip_add_numberTCs/auto_fill_number_TCs.sh
@@ -1,5 +1,14 @@
 #!bin/bash
-read -p "Nhap ten file c (vd: atest_zxc): " name_file
+echo 'Search file .c'
+name_file=`find . -type f -name '*.c'`
+if [ -f $name_file ]
+then
+	echo Running....
+	echo $name_file
+else 
+	exit
+fi
+
 rm -rf log_change.txt
 touch log_change.txt
 temp_count=1
@@ -7,10 +16,10 @@ temp_count=1
 echo -e 'Search for the word \e[30;48;5;82m"START_TEST("test_"    \e[0m'
 echo 'Search for the word "START_TEST("test_" ' >> log_change.txt
 # Search for the word "START_TEST("test_" 
-grep 'START_TEST("test_*' $name_file.c| cut -d '_' -f2 > temp_out1
+grep 'START_TEST("test_*' $name_file| cut -d '_' -f2 > temp_out1
 # Search for [line numbers] containing the word "START_TEST("test_" 
 # [cut -d ':' -f1] >> Filter the line numbers
-grep -n 'START_TEST("test_*' $name_file.c| cut -d ':' -f1 > temp_out2
+grep -n 'START_TEST("test_*' $name_file| cut -d ':' -f1 > temp_out2
 
 for i in `cat temp_out2`
 do
@@ -21,7 +30,7 @@ do
 	echo "===========> $Str_Replace" >> log_change.txt
 	echo Line $i: $Str_Pattern 
 	echo "===========> $Str_Replace"
-	sed -i "$i s/$Str_Pattern/$Str_Replace/" $name_file.c
+	sed -i "$i s/$Str_Pattern/$Str_Replace/" $name_file
 	((temp_count++))
 	
 done 
@@ -30,10 +39,10 @@ temp_count=1
 echo -e 'Search for the word \e[30;48;5;82m"START_TEST("[0-9]\{1,\}:"  \e[0m'
 
 echo 'Search for the word "START_TEST("[0-9]\{1,\}:" ' >> log_change.txt
-grep 'START_TEST("[0-9]\{1,\}:' $name_file.c| cut -d ' ' -f5 > temp_out1
+grep 'START_TEST("[0-9]\{1,\}:' $name_file| cut -d ' ' -f5 > temp_out1
 # Search for [line numbers] containing the word "START_TEST("[0-9]*:" 
 # [cut -d ':' -f1] >> Filter the line numbers
-grep -n 'START_TEST("[0-9]\{1,\}:' $name_file.c| cut -d ':' -f1 > temp_out2
+grep -n 'START_TEST("[0-9]\{1,\}:' $name_file| cut -d ':' -f1 > temp_out2
 
 for i in `cat temp_out2`
 do
@@ -46,13 +55,13 @@ do
 	echo "===========> $Str_Replace" >> log_change.txt
 	echo Line $i: $Str_Pattern 
 	echo "===========> $Str_Replace"
-	sed -i "$i s/$Str_Pattern/$Str_Replace/" $name_file.c
+	sed -i "$i s/$Str_Pattern/$Str_Replace/" $name_file
 	((temp_count++))
 	
 done 
 echo -e "\e[30;48;5;82m ===============Done============== \e[0m"
-rm -rf temp_out1
-rm -rf temp_out2
+rm -rf temp_out1 temp_out2
+
 read -p "Close sau 3s... Xem lai o log_change.txt" -t 3
 
 
