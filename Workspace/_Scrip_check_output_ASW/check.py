34753136f6f8935c2f0787ba0559fd59243b3582
EXTERNAL Nguyen Duy Bang (Ban Vien <nbg7hc@bosch.com>
2020-07-10 19:43:08 +0700

    ok

--- Workspace/_Scrip_check_output_ASW/check.py
+++ Workspace/_Scrip_check_output_ASW/check.py
@@ -4,9 +4,9 @@ import sys
 path_excel=str(sys.argv[1])
 wb = openpyxl.load_workbook(path_excel)
 sheet_Summary = wb['Summary']
-print("         C0 C1  :", sheet_Summary['A6'].value )
-print("         MCDC   :", sheet_Summary['B6'].value )
-print("         Reason :", sheet_Summary['C6'].value )
+print("                                 C0 C1  :", sheet_Summary['A6'].value )
+print("                                 MCDC   :", sheet_Summary['B6'].value )
+print("Reason :", sheet_Summary['C6'].value[0:50] )
 wb.close()
 
  
