da00ecfd715f2ea04f1f3a32ce4aeb58184fe622
Nguyen Duy Bang <bang.nguyen-duy@internal.banvien.com>
2021-05-21 13:14:57 +0700

    [Script][Updated] Updated Local Path 	- Cập nhật lại đường dẫn file. 	- Chuyển 2 file script cho Binance ra ngoài thư mục riêng.

--- _Quick_Buttons/Get_subSVN.bat
+++ _Quick_Buttons/Get_subSVN.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
 Rem Su dung `where bash` de tim duong dan Bash.exe
 @ECHO Running...
-"C:\Users\bang.nguyen-duy\AppData\Local\Programs\Git\usr\bin\bash.exe" "D:\Git_NDB\Documents\_Quick_Buttons\Get_subSVN.sh" %*
+"C:\Users\bang.nguyen-duy.BANVIENCORP\AppData\Local\Programs\Git\usr\bin\bash.exe" "D:\Git_NDB\Documents\_Quick_Buttons\Get_subSVN.sh" %*
 
 rem PAUSE
 
